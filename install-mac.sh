0f114e67c7836480d15ac3d8e93b81f2e65b994d
gooooloo <qidu.lin@gmail.com>
2016-04-23 14:39:31 +0800

    first template and install script

---
+++ install-mac.sh
@@ -0,0 +1,6 @@
+#!/bin/bash
+folderBase=~/Library/Preferences
+folderAndroidStudio=$folderBase/AndroidStudio2.0
+folderTemplate=$folderAndroidStudio/templates
+mkdir -p $folderTemplate
+ln -s ./gooooloo.xml $folderTemplate/gooooloo.xml
