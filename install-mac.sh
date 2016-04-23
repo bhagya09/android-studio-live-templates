7bd0d8bf5c020943abd7e7d637b82763f72282c4
gooooloo <qidu.lin@gmail.com>
2016-04-23 14:52:30 +0800

    fix bug of install-mac.sh

--- install-mac.sh
+++ install-mac.sh
@@ -3,4 +3,4 @@ folderBase=~/Library/Preferences
 folderAndroidStudio=$folderBase/AndroidStudio2.0
 folderTemplate=$folderAndroidStudio/templates
 mkdir -p $folderTemplate
-ln -s ./gooooloo.xml $folderTemplate/gooooloo.xml
+ln -s ${PWD}/gooooloo.xml $folderTemplate/gooooloo.xml
