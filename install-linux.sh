63cde63f61698d1df7aa921db2e6b383edbefaae
gooooloo <qidu.lin@gmail.com>
2016-06-27 18:28:42 +0800

    install script for different AndroidStudio versions for linux

--- install-linux.sh
+++ install-linux.sh
@@ -1,5 +1,9 @@
 #!/bin/bash
-folderAndroidStudio=~/.AndroidStudio2.0/config/
-folderTemplate=$folderAndroidStudio/templates
-mkdir -p $folderTemplate
-ln -s ${PWD}/gooooloo.xml $folderTemplate/gooooloo.xml
+for i in ~/.AndroidStudio*
+do
+    echo $i
+    folderAndroidStudio=$i/config/
+    folderTemplate=$folderAndroidStudio/templates
+    mkdir -p $folderTemplate
+    ln -s ${PWD}/gooooloo.xml $folderTemplate/gooooloo.xml
+done
